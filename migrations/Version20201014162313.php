ad97083c3c249181f0d9786f5b5cceecf493f014
ben <toufik-m@live.fr>
2020-10-15 00:45:56 +0200

    dependance injection pins ok

--- migrations/Version20201014162313.php
+++ migrations/Version20201014162313.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201014012121 extends AbstractMigration
+final class Version20201014162313 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Create pins table';
     }
 
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE pin (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pins (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE pin');
+        $this->addSql('DROP TABLE pins');
     }
 }
--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,9 +14,16 @@ class PinsController extends AbstractController
     /** 
     *@Route("/")
     */
-   
-    public function __invoke(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $pin = new Pin;
+         $pin->setTitre('Title 1');
+         $pin->setDescription('Description 1');
+
+     
+        $em->persist($pin);
+        $em->flush();
+
         return $this->render('pins/index.html.twig');
     }
 }
